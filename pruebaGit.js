2024fec8957c752a7592c32fc41d7a5f77cd6381
kbcruz6 <agustintcruz@gmail.com>
2022-08-16 14:59:07 -0300

    pages

--- pruebaGit.js
+++ pruebaGit.js
@@ -1,4 +1,20 @@
 
+// // BOTONES ELECCION
+
+// function showPage(page){
+//     document.querySelectorAll("div").forEach(div=>{
+
+//     div.style.display= "none";
+//     })
+//     document.querySelector(`#${page}`).style.display="block";
+// }
+// document.addEventListener("DOMContentLoaded", function(){
+//     document.querySelectorAll("button").forEach(button=>{
+//         button.onclick=function(){
+//             showPage(this.dataset.page); 
+//         }
+//     });
+// });
 
 // CRONOMETRO TIEMPO EN WEB
 document.addEventListener("DOMContentLoaded",()=>{
