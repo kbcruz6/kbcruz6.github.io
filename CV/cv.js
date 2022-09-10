a64262c6f52e8f4f5e70d036962cd06b0e8876e6
kbcruz6 <agustintcruz@gmail.com>
2022-09-10 14:34:14 -0300

    paletita de colores

--- CV/cv.js
+++ CV/cv.js
@@ -1,9 +1,44 @@
 
+// CHANGE LANGUAGE
+
+let enEs=document.querySelector(".enEs");
+let esEn=document.querySelector(".esEn");
+let idioma1=document.querySelector(".eng");
+let idioma2=document.querySelector(".spani");
+
+enEs.addEventListener("click",function(){
+    idioma1.classList.toggle("invisible");
+    idioma2.classList.toggle("invisible");
+});
+
+esEn.addEventListener("click",function(){
+    idioma1.classList.toggle("invisible");
+    idioma2.classList.toggle("invisible");
+});
+
+// PALETA DE COLORES
+
+let paleta=document.getElementById("toggle");
+let body=document.body;
+
+paleta.addEventListener("click",function(){
+    body.classList.toggle("dark");
+});
+
+let paleta2=document.getElementById("toggle2");
+
+
+paleta2.addEventListener("click",function(){
+    body.classList.toggle("dark");
+});
+
 const today=()=>{
     let todayDate = new Date().toLocaleDateString();
     let todayTime = new Date().toLocaleTimeString();
 
-    document.querySelector(".bd").innerHTML=`My Age today ${todayDate} at ${todayTime} hs:<br>`;
+    document.querySelector(".bd1").innerHTML=`My Age today ${todayDate} at ${todayTime} hs:<br>`;
+    document.querySelector(".bd2").innerHTML=`My Age today ${todayDate} at ${todayTime} hs:<br>`;
+
 }
 const age=()=>{
 
@@ -30,16 +65,9 @@ const age=()=>{
         dias="day"
     } 
 
-    document.querySelector(".age").innerHTML=`${years} years, ${months} ${meses}, ${days} ${dias}`
+    document.querySelector(".age1").innerHTML=`${years} years, ${months} ${meses}, ${days} ${dias}`;
+    document.querySelector(".age2").innerHTML=`${years} years, ${months} ${meses}, ${days} ${dias}`;
+
 }
 setInterval(today,1000);
 age(); 
-
-// PALETA DE COLORES
-
-let paleta=document.getElementById("toggle");
-let body=document.body;
-
-paleta.addEventListener("click",function(){
-    body.classList.toggle("dark");
-});
