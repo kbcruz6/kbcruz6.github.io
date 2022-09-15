d145605f688aa174c54804e7b0268dee28d9c54c
kbcruz6 <agustintcruz@gmail.com>
2022-09-15 17:40:44 -0300

    copy

--- coallition/code.js
+++ coallition/code.js
@@ -1,40 +1,37 @@
 
-let paleta=document.getElementById("toggle1b");
+let paleta=document.getElementById("toggle1");
 let paleta2=document.getElementById("toggle2");
 
 let mont1=document.querySelector(".mountain-1");
-let mont1b=document.querySelector(".mont1b-cont");
+let mont2=document.querySelector(".mountain-2");
 
-let mont2=document.querySelector(".mont2-cont");
-let mont2b=document.querySelector(".mountain-2b");
+let mont1Cont=document.querySelector(".mont1-cont");
+let mont2Cont=document.querySelector(".mont2-cont");
 
-let rect1=document.querySelector(".rect-m1");
-let rect2=document.querySelector(".rect-m2");
-
-let tab1=document.querySelector(".tab-1");
-let tab2=document.querySelector(".tab-2");
+let pag2a=document.querySelector(".pag2a");
+let pag2b=document.querySelector(".pag2b");
 
 paleta.addEventListener("click",function(event){
-    mont1.classList.toggle("hidden");
-    mont1b.classList.toggle("hidden");
-    mont2.classList.toggle("hidden");
-    mont2b.classList.toggle("hidden");
-    rect1.classList.toggle("hidden");
-    rect2.classList.toggle("hidden");
-    tab1.classList.toggle("hidden");
-    tab2.classList.toggle("hidden");
+    mont1.classList.toggle("colour1");
+    mont2.classList.toggle("colour2");
+
+    mont1Cont.classList.toggle("colour1c");
+    mont2Cont.classList.toggle("colour2c");
+
+    pag2a.classList.toggle("hidden");
+    pag2b.classList.toggle("hidden");
     event.preventDefault();
 });
 
 paleta2.addEventListener("click",function(event){
-    mont1.classList.toggle("hidden");
-    mont1b.classList.toggle("hidden");
-    mont2.classList.toggle("hidden");
-    mont2b.classList.toggle("hidden");
-    rect1.classList.toggle("hidden");
-    rect2.classList.toggle("hidden");
-    tab1.classList.toggle("hidden");
-    tab2.classList.toggle("hidden");
+    mont1.classList.toggle("colour1");
+    mont2.classList.toggle("colour2");
+
+    mont1Cont.classList.toggle("colour1c");
+    mont2Cont.classList.toggle("colour2c");
+
+    pag2a.classList.toggle("hidden");
+    pag2b.classList.toggle("hidden");
     event.preventDefault();
 });
 
@@ -49,3 +46,6 @@ paleta2.addEventListener("click",function(event){
 //     overflow: 'auto',
 //     height: 'auto'
 // });
+
+//     --color1:rgb(65, 79, 107);
+// --color2:rgb(176, 180, 190);
