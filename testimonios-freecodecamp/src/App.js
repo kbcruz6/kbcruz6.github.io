a3d54d47159bc9d12063fed6ac48c0e44ea9f66f
kbcruz6 <agustintcruz@gmail.com>
2022-09-18 20:42:45 -0300

    text b

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -14,24 +14,21 @@ function App() {
           imagen="agus"
           cargo="Ingeniero Industrial"
           empresa="Google"
-          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa."
-        />
+          testimonio={<text>`Lorem ipsum dolor <strong> sit amet</strong> consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa.`</text>} />
         <Testimonio 
           nombre="Gonzalo Blotta"
           pais="Argentina"
           imagen="gonza"
           cargo="Full Stack Developer"
           empresa="XOOR"
-          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa."
-        />
+          testimonio={<text>`Lorem ipsum <b> dolor sit amet</b> consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa.`</text>} />
         <Testimonio 
           nombre="Maria Fernanda Cruz"
           pais="Argentina"
           imagen="fer"
           cargo="Tatuadora"
           empresa="Waldorf Astoria"
-          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa."
-        />
+          testimonio={<text>`Lorem ipsum <b>dolor sit amet consectetur adipisicing</b> elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa.`</text>} />
       </div>
     </div>
   );
--- testimonios-freecodecamp/src/componentes/Testimonios.js
+++ testimonios-freecodecamp/src/componentes/Testimonios.js
@@ -4,10 +4,10 @@ import "../hojas-de-estilo/Testimonio.css";
 export function Testimonio(props){
   return(
     <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require(`../Imagenes/testimonio-${props.imagen}.jpg`)} alt="Foto Agus" />
+      <img className="imagen-testimonio" src={require(`../Imagenes/testimonio-${props.imagen}.jpg`)} alt={`Foto de ${props.nombre}`} title={`Foto de ${props.nombre}`} />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
-        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio"><strong>{props.cargo}</strong> en {props.empresa}</p>
         <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
