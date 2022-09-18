f335b5c5731a0c5c32e45da431c8f78b1f4c285c
kbcruz6 <agustintcruz@gmail.com>
2022-09-18 19:51:00 -0300

    test

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import {Testimonio} from "./componentes/Testimonios";
+import { logRoles } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
       <div className='contenedor-principal'>
-        <h1>Esto es lo que nuestros alumnos dicen de Free Code Camp</h1>
-        <Testimonio/>
+        <h1>Testimonios de alumnos</h1>
+        <Testimonio 
+          nombre="Agustin Cruz"
+          pais="Argentina"
+          imagen="agus"
+          cargo="Ingeniero Industrial"
+          empresa="Google"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa."
+        />
+        <Testimonio 
+          nombre="Gonzalo Blotta"
+          pais="Argentina"
+          imagen="gonza"
+          cargo="Full Stack Developer"
+          empresa="XOOR"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa."
+        />
+        <Testimonio 
+          nombre="Maria Fernanda Cruz"
+          pais="Argentina"
+          imagen="fer"
+          cargo="Tatuadora"
+          empresa="Waldorf Astoria"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis esse quaerat qui repudiandae illo dolore placeat accusantium eligendi facere? Ipsum vero molestias quo illum dolores assumenda temporibus, veniam molestiae culpa."
+        />
       </div>
     </div>
   );
--- testimonios-freecodecamp/src/componentes/Testimonios.js
+++ testimonios-freecodecamp/src/componentes/Testimonios.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../hojas-de-estilo/Testimonio.css";
 
-export function Testimonio(){
+export function Testimonio(props){
   return(
     <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require("../Imagenes/testimonio-agus.jpg")} alt="Foto Agus" />
+      <img className="imagen-testimonio" src={require(`../Imagenes/testimonio-${props.imagen}.jpg`)} alt="Foto Agus" />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Agustin Cruz en Argentina</p>
-        <p className="cargo-testimonio">Ingeniero Industrial y Frontend Developer</p>
-        <p className="texto-testimonio">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, dolorem quo illum sunt tenetur quis autem hic, incidunt, assumenda vitae explicabo fugit dolor harum quia eveniet saepe ratione rerum libero?</p>
+        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
 }
+
