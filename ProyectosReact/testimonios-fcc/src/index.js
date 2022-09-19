de155f9ddc9dde6ba6e3eb111c9426e55b8c50b7
kbcruz6 <agustintcruz@gmail.com>
2022-09-19 12:45:08 -0300

    index js

--- ProyectosReact/testimonios-fcc/src/index.js
+++ ProyectosReact/testimonios-fcc/src/index.js
@@ -3,9 +3,11 @@
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
